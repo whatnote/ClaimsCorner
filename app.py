d350214c71a10820af794151f059f187ccedd220
DANIEL <daniel_cornwall@hotmail.com>
2021-10-20 01:44:57 +0000

    almost connected to DB

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
+
 
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "Hello claims corner"
+@app.route("/get_claim_form")
+def get_claim_form():
+    claim_form = mongo.db.tasks.find()
+    return render_template("claim_form.html", claim_form=claim_form)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")), 
-            debug=True)  
+            port=int(os.environ.get("PORT")),
+            debug=True)
 # change this to false when submitting the project
